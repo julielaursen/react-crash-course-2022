3b1c12acf4d6f6c6f2ed999fb0a91f71e4f5cce8
julielaursen <julielaursen1@gmail.com>
2022-10-23 22:54:09 -0500

    working commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  // stopped at 33:33 on https://www.youtube.com/watch?v=b9eMGE7QtTk
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>-</button>
+      <h1>{counter}</h1>
+      <button>+</button>
     </div>
   );
 }
